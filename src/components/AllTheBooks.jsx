20b7df880307a01c588e737fecc920b14576293d
Giuseppe Canzoneri <gcanzoneri6@gmail.com>
2023-03-29 17:25:57 +0200

    commit

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,17 +1,24 @@
 import { Component } from "react";
 import { Container, Row, Col, Card, Badge, Button } from "react-bootstrap";
-import books from "../books/fantasy.json";
+import fantasy from "../books/fantasy.json";
+import history from "../books/history.json";
+import horror from "../books/horror.json";
+import romance from "../books/romance.json";
+import scifi from "../books/scifi.json";
 
 class AlltheBooks extends Component {
-  //   state = {
-  //     selectedBooks: null,
-  //   };
+  state = {};
+  getRandomNumber = () => {
+    return Math.random() - 0.5;
+  };
 
   render() {
+    const allTheBooks = [...fantasy, ...history, ...horror, ...romance, ...scifi];
+    allTheBooks.sort(this.getRandomNumber);
     return (
       <Container className="mt-3 bg-dark">
         <Row xs={1} sm={2} md={3} lg={4} xl={5} xxl={6}>
-          {books.map(cover => (
+          {allTheBooks.map(cover => (
             <Col key={cover.asin} className="mb-4">
               <Card className="bg-secondary">
                 <Card.Img variant="top" src={cover.img} />
